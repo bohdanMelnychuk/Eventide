ebd5fbaad198d9fd44de31b9c253190886e08fc5
Пользователь <>
2016-12-05 01:33:52 +0200

    validation forms and plugin select2

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,23 @@ $(function(){
     $('.collapse').on('show.bs.collapse',function(){
         $(this).prev().find('.fa').removeClass().addClass('fa fa-minus-circle');
     });
+    
     $('.collapse').on('hide.bs.collapse',function(){
         $(this).prev().find('.fa').removeClass().addClass('fa fa-plus-circle');
     });
+    
     $('.carousel').carousel({
       interval: false
     });
+    
+    $('#myForm').validator({
+        feedback: {
+            success: 'fa-check-circle',
+            error: 'fa-times-circle'
+        }
+    });
 	
+    $('.select-item').select2({
+        minimumResultsForSearch: Infinity
+    });
 });

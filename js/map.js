57ba545ed2f98ce85784669332d45e9cef669458
Пользователь <>
2016-12-05 10:44:36 +0200

    add partner and google maps

---
+++ js/map.js
@@ -0,0 +1,15 @@
+"use strict";
+function myMap(){
+    var myCenter = new google.maps.LatLng(49.858742,24.020850);
+    var mapCanvas = document.getElementById('map');
+    var mapOptions = {
+    center: myCenter,
+    zoom: 5
+};
+    var  map = new google.maps.Map(mapCanvas,mapOptions);
+    var marker = new google.maps.Marker({
+        position:myCenter,
+        animation: google.maps.Animation.BOUNCE
+    });
+    marker.setMap(map);
+}
